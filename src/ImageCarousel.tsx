df9a36e3c590eb45dde35b5d8564571c1c2815c5
topshenyi-web <erxu@ctemc.org>
2025-06-09 16:16:54 -0400

    style: image carousel

--- src/ImageCarousel.tsx
+++ src/ImageCarousel.tsx
@@ -1,15 +1,21 @@
 import css from "./ImageCarousel.module.css";
 
 export default function ImageCarousel() {
-  console.log(
+  const imageLinks: string[] = Object.values(
     import.meta.glob("./assets/carousel/*", {
       eager: true,
       query: "url",
-    })
-  );
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    }) as Record<string, any>
+  ).map((data) => data.default);
+
   return (
-    <div className="centered-container">
-      <div className={css["carousel"]}></div>
+    <div className="centered-section">
+      <div className={css["carousel"]}>
+        {imageLinks.map((url) => (
+          <img className={css["carousel__image"]} src={url} alt="" />
+        ))}
+      </div>
     </div>
   );
 }
